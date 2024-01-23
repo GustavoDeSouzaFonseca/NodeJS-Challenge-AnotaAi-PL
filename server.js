f0f427fb7b9a68776abc6370108beb113f5c3089
GustavoDeSouzaFonseca <rm94067@fiap.com.br>
2024-01-23 10:50:04 -0300

    [ :construction: ] Initializing the node project using express

---
+++ server.js
@@ -0,0 +1,6 @@
+import app from "./src/app.js";
+
+const PORT = process.env.PORT || 8080;
+const BASE_URL = process.env.BASE_URL || 'http://localhost'
+
+app.listen(PORT, () => console.log(`Listening at ${BASE_URL}:${PORT}`));
